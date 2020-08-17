67c7985fbdab27d950034f9c56347103c8097104
Nanda Gopal <31374517+Nanda-G@users.noreply.github.com>
2020-08-17 18:36:48 +0530

    Update script to automatically find maps folder.

--- kzmap.sh
+++ kzmap.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-echo $1
-echo "$1"
-MAP="https://fastdlv2.gflclan.com/file/gflfastdl/csgo/maps/$1.bsp"
-echo $MAP
-if curl --output /dev/null --silent --head --fail "$MAP"; then
-    wget ${MAP}
-    mv $1.bsp ./.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/maps/
-else
-    echo "No file."
-    wget https://fastdlv2.gflclan.com/file/gflfastdl/csgo/maps/"$1".bsp.bz2
-    bzip2 -d $1.bsp.bz2
-    mv $1.bsp ./.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/maps/
-    rm $1.bsp.bz2
-fi
+ #!/bin/bash
+ MAP="https://fastdlv2.gflclan.com/file/gflfastdl/csgo/maps/$1.bsp"
+ echo "Map to be downloaded:"
+ echo $MAP
+ echo "Finding CS:GO maps directory...."
+ maps_dir=$(find / -print 2>/dev/null | grep -s csgo/maps | head -n 1)
+ echo "Directory found at: $maps_dir"
+ if curl --output /dev/null --silent --head --fail "$MAP"; then
+     wget ${MAP}
+     mv $1.bsp "$maps_dir"
+ else
+     echo "No file."
+     wget https://fastdlv2.gflclan.com/file/gflfastdl/csgo/maps/"$1".bsp.bz2
+     bzip2 -d $1.bsp.bz2
+     mv $1.bsp "$maps_dir"
+ fi
